6c496cc172071f24d4d177db0083cf7791cc42c9
Nguyen Nam <60035507+tavenguyen@users.noreply.github.com>
2025-12-06 19:15:14 +0700

    chore: [DEBUG] all tests for debugging.

--- hash_table.cpp
+++ hash_table.cpp
@@ -2,14 +2,19 @@
 #include "hash_table.h"
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
 int main() {
     ht_hash_table<int> ht(10);
-    ht.insert("User1", 100);
-    ht.insert("User2", 200);
-    ht.insert("User3", 300);
-    std::cout << *ht.get("User2") << std::endl;
-    ht.insert("User1", 500);
-    std::cout << *ht.get("User1") << std::endl;
+    for(int i = 0; i < 1000; i++) {
+        std::string result = "Key" + std::to_string(i + 1);
+        ht.insert(result, i);
+    }
+    ht.remove("Key25");
+    assert(ht.find("Key25") == -1);
+    assert(ht.find("Key26") != -1);
+    ht.insert("Key25", 999);
+    assert(ht.find("Key25") != -1);
+    std::cout << "ALL TESTS PASSED!" << std::endl;
     return 0;
 }
